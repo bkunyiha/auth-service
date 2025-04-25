00d2db421be7b0acf214531aca620da59a550297
Bill Kunyiha <bkunyiha@gmail.com>
2025-04-25 07:23:25 -0700

    Sprint 3 - Task 5

--- auth-service/tests/api/logout.rs
+++ auth-service/tests/api/logout.rs
@@ -115,3 +115,48 @@ async fn should_return_400_if_logout_called_twice_in_a_row() {
     let response = app.post_logout(&logout_request).await;
     assert_eq!(response.status().as_u16(), 400);
 }
+
+#[tokio::test]
+async fn verify_token_added_to_banned_token_store_after_logout() {
+    let app = TestApp::new().await;
+
+    let email_str: String = SafeEmail().fake();
+    let password_str: String = FakerPassword(std::ops::Range {start: 8, end: 30}).fake();
+       
+    // Signup a new user
+    let signup_request = serde_json::json!({
+        "email": email_str,
+        "password": password_str,
+        "requires2FA": true
+    });
+    let response = app.post_signup(&signup_request).await;
+    assert_eq!(response.status().as_u16(), 201);
+
+    // Login the user
+    let login_request = serde_json::json!({
+        "email": email_str,
+        "password": password_str,
+    });
+    let response = app.post_login(&login_request).await;
+    assert_eq!(response.status().as_u16(), 200);
+
+    // Get the auth cookie from login response
+    let auth_cookie = response
+        .cookies()
+        .find(|cookie| cookie.name() == JWT_COOKIE_NAME)
+        .expect("No auth cookie found");
+    assert!(!auth_cookie.value().is_empty());
+    let login_token = auth_cookie.value();
+
+    // Logout to add the token from the banned token store
+    let logout_request = serde_json::json!({
+        "email": email_str,
+        "password": password_str,
+    });
+    let response = app.post_logout(&logout_request).await;
+    assert_eq!(response.status().as_u16(), 200);
+
+    // Verify the token added to banned token store after logout
+    let banned_token_store = app.banned_token_store.read().await;
+    assert!(banned_token_store.get_token(&login_token.to_string()).await.is_ok());
+}
